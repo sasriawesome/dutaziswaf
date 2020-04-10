d37ff4157708061eb11f27da40df7d03906b8014
sasriawesome <sasri.project@gmail.com>
2020-04-10 13:45:15 +0700

    update setting

--- playground/settings/base.py
+++ playground/settings/base.py
@@ -171,7 +171,7 @@ STATICFILES_DIRS = [
 # See https://docs.djangoproject.com/en/3.0/ref/contrib/staticfiles/#manifeststaticfilesstorage
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
